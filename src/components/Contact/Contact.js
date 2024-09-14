49cadcba63bd2a5f654a88db8685d2ef8174c283
vignesh <vigneshviki1x@gmail.com>
2024-09-14 18:55:34 +0530

    portfolio

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,7 @@ import "./_footer.scss";
 function Contact(props) {
   const details = {
     resumeLink:
-      "https://drive.google.com/file/d/1hhGGqzPpChgz085c7iJS_GLeAXEZolhu/view",
+      "https://drive.google.com/file/d/1YUk1EDlaHudPYp_4iYpwd9DAt-FSMzLE/view?usp=drive_link",
     phone: "Phone : 9360162026",
     email: "Email: vigneshviki1x@gmail.com",
   };
